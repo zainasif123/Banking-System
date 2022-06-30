51ea7fde23d7cce993fc03fdcfb01037a6530feb
Ghulam Abbas <102281059+shani710@users.noreply.github.com>
2022-06-30 12:56:56 +0500

    Sql Connection
    add sql connection

---
+++ Code/Banking_System/src/banking_system/dal/DALManager.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package banking_system.dal;
+
+/**
+ *
+ * @author shanii
+ */
+public class DALManager {
+    
+}
