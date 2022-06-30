0bb557d042a8886259c8dfc690fbe413c64a1ad6
Hassanjaved4157 <Hassanjaved4157@gmail.com>
2022-06-30 15:10:01 +0500

    databaseFolder
    hello

---
+++ database/SQLQuery4.sql
@@ -0,0 +1,3 @@
+--create table bank_deatils(Customer_id int primary key identity(1,1), Cnic varchar(25) not null, password varchar(25) not null)
+--insert into bank_deatils(Cnic,password) values('34101-4972952-3','25272634')
+select * from bank_details
