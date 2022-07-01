1daaec80987fe2aa8afa324816eaad1f57fa3b6c
ahmedbinkhalid <ahmed321binkhalid@gmail.com>
2022-07-01 08:44:53 +0500

    changes
    dsbbsb

--- database/SQLQuery1.sql
+++ database/SQLQuery1.sql
@@ -1,5 +1,1 @@
---Insert Into Accountant(Cnic,Name,fatherName,password,confirmPassword) values(34101,'Ahmed Bin Khalid','Khalid Javed',223344,223344)
---Insert Into Accountant(Cnic,Name,fatherName,password,confirmPassword) values(34202,'Rahim Durani','Tashreef Durani',115566,115566)
---Insert Into Accountant(Cnic,Name,fatherName,password,confirmPassword) values(341303,'Zain Asif','Mohammad Asif',212121,212121)
---Insert Into Accountant(Cnic,Name,fatherName,password,confirmPassword) values(34404,'Hassan Javed','Javed Iqbal',25272634,25272634)
-select * from Accountant
+select * from bnakEmployees
